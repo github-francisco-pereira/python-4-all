73c468b796227b5c316e1830ab007a862c8cfbe7
shikow <shikowsp@gmail.com>
2019-07-17 11:15:35 -0300

    Add first class

---
+++ aula_1/exemplos.py
@@ -0,0 +1,17 @@
+from requests import get as get_do_chico # exemplo com alias
+from requests import post # import separado
+from requests import put # import separado
+
+from requests import get, put, post # import linha unica
+from requests import * # NAOOOO
+import requests # Implicito tb nao
+
+
+def xablau():
+    print('Chicote') # emitindo msg
+    variavel = 1 # atribuindo variavel
+    variavel_2 = None # atribuindo um nulo
+    variavel_3 = 4 * 2 #fazendo uma operação
+
+def main():
+    xablau()
---
+++ aula_1/exercicios.md
@@ -0,0 +1,7 @@
+## 1 - Exibir no console o campo uuid
+Crie um arquivo `.py` que efetue uma requisição web (GET) no endereço `https://httpbin.org/uuid` e exiba no console o campo `uuid`.
+Lembre-se de adicionar o `requirements.txt`.
+
+## 2 - Fizzbuzz
+Crie um arquivo `.py` que contenha uma função que receba um número `inteiro` e se o número for divisivel por 3 você exibe `fizz` no console. Se o número for divisivel por 5 você exibe `buzz`, caso seja por 5 e 3 você exibe `fizzbuzz`.
+
