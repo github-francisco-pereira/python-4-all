980e1c0f4aea8377d5241af5502ea5e25169d70f
Ana Carolina Lima Hamud <ana.hamud@gmail.com>
2019-07-25 16:27:23 -0300

    Fix exercise 1

--- aula_1/anahamud/exercicio1/main.py
+++ aula_1/anahamud/exercicio1/main.py
@@ -1,3 +1,10 @@
+# virtualenv venv
+# source venv/bin/activate
+# pip install requests
+# python main.py
+# pip freeze -l > requirements.txt
+# deactivate
+
 import requests
 
 response = requests.get('https://httpbin.org/uuid')
