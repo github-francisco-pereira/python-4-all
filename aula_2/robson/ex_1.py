839dde9759c5d59f803c5fab76acb65cd64606c7
Robson Duarte <robson.o.d@gmail.com>
2019-08-06 22:39:31 -0300

    simplify the ex 1

--- aula_2/robson/ex_1.py
+++ aula_2/robson/ex_1.py
@@ -1,5 +1,3 @@
-odd_numbers = [ number for number in range(0, 1000) if not number % 2 == 0]
-
-result = sum(odd_numbers)
+result = sum([ number for number in range(0, 1000) if not number % 2 == 0])
 
 print(result)
