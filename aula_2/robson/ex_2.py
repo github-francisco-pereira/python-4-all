1cbfd50e8b42fdc5f1ce6451c636d9e7cfc2f0f9
robsonoduarte <robson.duarte@grupozap.com>
2019-08-07 18:18:08 -0300

    ex 2

--- aula_2/robson/ex_2.py
+++ aula_2/robson/ex_2.py
@@ -1,6 +1,16 @@
-number = range(1,10)
+from sympy import isprime
 
 def find_prime_number(list):
-    x = False
-    while not x:
-        
+    i = 0
+    while i <= len(list) and not isprime(list[i]):
+        i = i + 1
+    
+    return list[i]
+
+
+print(find_prime_number(range(10)))    
+
+# Como nada se cria tudo se copia :)
+# pip3 install sympy
+
+
