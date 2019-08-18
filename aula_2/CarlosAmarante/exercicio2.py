7e33e37ad22d112a74482c7a4454b970be34d946
carloosamarante <amarante.carlos@hotmail.com>
2019-08-18 14:29:39 -0300

    Fix exercise 2

--- aula_2/CarlosAmarante/exercicio2.py
+++ aula_2/CarlosAmarante/exercicio2.py
@@ -1,20 +1,18 @@
 def is_prime(current_number):
-    if current_number <= 1:
+    if current_number == 1:
         return False
-    else:
-        auxiliar_number = current_number - 1
-        auxiliar_count = 2
-        while (1 < auxiliar_count and auxiliar_count <= auxiliar_number):
-            if current_number % auxiliar_count == 0:
-                return False
-            auxiliar_count += 1
-        print(current_number)
+    elif current_number == 2:
         return True
+    else:
+        for ranged_number in range(2, current_number):
+            return current_number % ranged_number != 0
 
 
-x = list(range(1, 11))
+LIST_NUMBER = list(range(1, 11))
 get_out = False
 count = 0
-while count < x[-1] and get_out is not True:
-    is_prime(x[count])
+while count < LIST_NUMBER[-1] and get_out is not True:
+    if is_prime(LIST_NUMBER[count]):
+        print(LIST_NUMBER[count])
+        get_out = True
     count += 1
